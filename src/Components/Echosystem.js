84ec2d9638e572202dcef2f3c9cfe92eff0476c1
DefiCody <ambergridpro@gmail.com>
2023-06-22 22:16:53 +0530

    ok

--- src/Components/Echosystem.js
+++ src/Components/Echosystem.js
@@ -15,36 +15,36 @@ const Echosystem = () => {
           <div className="mobileAdjust gap-4">
             <Card className="echocard cls">
               <Card.Body>
-                <h2 className="marketboxH text-center">Ordinals</h2>
+                <h2 className="marketboxH text-center">Buy/sell Tax</h2>
                 <p className="coinP text-center">
-                  Universe of collections interlinked to one another through
-                  storytelling.
+                Buy and sell tax will be 0 forever.
                 </p>
                 <ul>
-                  <li className="ul">Collection 1:</li>
+                  <li className="ul">BUY TAX:
+</li>
                 </ul>
-                <p className="ulitem">Miniature Alpha Males : 650</p>
+                <p className="ulitem">0 % Buy tax</p>
                 <ul>
-                  <li className="ul">Collection 2:</li>
+                  <li className="ul">SELL TAX:</li>
                 </ul>
-                <p className="ulitem">Miniature Beta Females : TBA</p>
+                <p className="ulitem">0% Sell tax</p>
                 <ul>
-                  <li className="ul">Collection 3:</li>
+                  <li className="ul">RENOUNCE OWNERSHIP:</li>
                 </ul>
-                <p className="ulitem">Miniature Mutant Babies: TBA</p>
+                <p className="ulitem">THE ownership will be Renounced on Launch</p>
               </Card.Body>
             </Card>
             <Card className="margintopA echocard cls">
               <Card.Body>
-                <h2 className="marketboxH text-center">Brc-20</h2>
+                <h2 className="marketboxH text-center">$BAIT</h2>
                 <p className="coinP text-center">
                   Native token powering the ecosystem with multiple use cases.
                 </p>
-                <p className="ulbelow">21M supply</p>
+                <p className="ulbelow">5M Total Supply</p>
                 <ul className="echo2bullet">
-                  <li>45% Holders claim </li>
-                  <li>45% Collabs & Partners </li>
-                  <li>20% Team & Treasury</li>
+                  <li>60% Presale  </li>
+                  <li>30% Liquidity </li>
+                  <li>10% Marketing</li>
                 </ul>
                 <p className="ulbelow mt-2">Use Cases</p>
                 <button className="echobutton">
@@ -56,18 +56,18 @@ const Echosystem = () => {
             </Card>
             <Card className="margintopA echocard cls">
               <Card.Body>
-                <h2 className="marketboxH text-center">Passive Income</h2>
+                <h2 className="marketboxH text-center">LIQUIDITY</h2>
 
                 <p className="coinP text-center">
-                  Regular monetary reward distribution.
+                100% of liquidity will be locked.
                 </p>
-                <p className="ulbelow mt-5">Miniature auction platform</p>
+                <p className="ulbelow mt-5">90% Of supply </p>
                 <button className="echobutton">
                   {" "}
                   <img src={lock} />
                   Locked
                 </button>
-                <p className="ulbelow mt-3">Staking rewards</p>
+                <p className="ulbelow mt-3">10% of Marketing </p>
                 <button className="echobutton mt-0">
                   {" "}
                   <img src={lock} />
